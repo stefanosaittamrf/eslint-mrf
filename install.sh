ab6eb21d82ac6828fb0d47b883bafa6cbb64937e
Miquel Masriera <miquel.masriera@marfeel.com>
2017-01-17 13:03:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
-npm i -g eslint
-npm i -g eslint-config-google
+# gloabally install the latest verions of eslint and the google config
+# at the moment eslint 3.13 and google-config 0.7
+npm i -g eslint eslint-config-google
 
 # check
 npm -g ls eslint eslint-config-google
-
-# version
-eslint -v
